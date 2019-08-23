768cbc5fa4e806b0d6554f260e4c3909ad59a28e
Ondrej Urik <ondrej.urik@live.com>
2019-08-23 14:15:55 +0100

    tdd headers phase 1

--- csharp/tdd-headers/tdd.headers/Headers.cs
+++ csharp/tdd-headers/tdd.headers/Headers.cs
@@ -1,9 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace tdd.headers
 {
     public class Headers
     {
+        private readonly Dictionary<string, string> _headers = new Dictionary<string, string>();        
+        public string this[string field]
+        {
+            get
+            {
+                return _headers.ContainsKey(field.ToLower())
+                    ? _headers[field.ToLower()]
+                    : null;
+            }
+            set
+            {
+                if (field == null)
+                {
+                    throw new ArgumentNullException();
+                }
+                _headers[field.ToLower()] = value;
+            }
+        }
 
+        public int Count { get => _headers.Count; }
+
+        public void Add(string v1, string v2)
+        {
+            this[v1] = v2;
+        }
     }
 }
