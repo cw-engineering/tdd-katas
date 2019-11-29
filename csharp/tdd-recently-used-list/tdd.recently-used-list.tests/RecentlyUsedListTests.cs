3ee200cc6234de304110d8bc6dde4ad2ad273bc6
Ondrej Urik <ondrej.urik@live.com>
2019-11-29 15:00:29 +0000

    MRU tests dotnet core 3 stage I

--- csharp/tdd-recently-used-list/tdd.recently-used-list.tests/RecentlyUsedListTests.cs
+++ csharp/tdd-recently-used-list/tdd.recently-used-list.tests/RecentlyUsedListTests.cs
@@ -16,5 +16,70 @@ namespace Tdd.Collections.Tests
             iList.ShouldNotBeNull();
         }
 
+        [Test]
+        public void Add_WhenCalledOnce_ShouldReturnAddedItem()
+        {
+            var list = new RecentlyUsedList<int>();
+
+            list.Add("someId", 123);
+
+            list[0].Value.ShouldBe(123);
+        }
+
+        [Test]       
+        public void Add_WhenCalledTwiceWithDifferentValues_ShouldContainTwoItems()
+        {
+            var list = new RecentlyUsedList<int>();
+
+            list.Add("someId", 123);
+            list.Add("secondId", 123);
+
+            list.Count.ShouldBe(2);
+        }
+
+        [Test]
+        public void Add_WhenCalledThreeTimesWithDifferentValues_ShouldContainThreeItems()
+        {
+            var list = new RecentlyUsedList<int>();
+
+            list.Add("someId", 123);
+            list.Add("secondId", 123);
+            list.Add("threeId", 123);
+
+            list.Count.ShouldBe(3);
+        }
+
+        [Test]
+        public void Add_WhenCalledTwiceWithSameValues_ShouldOnlyAddOne()
+        {
+            var list = new RecentlyUsedList<int>();
+
+            list.Add("someId", 123);
+            list.Add("someId", 123);
+
+            list.Count.ShouldBe(1);
+        }
+
+        [Test]
+        public void Add_WhenCalledTwiceWithTheSameId_ShoudContainSecondItem()
+        {
+            var list = new RecentlyUsedList<int>();
+
+            list.Add("someId", 123);
+            list.Add("someId", 124);
+
+            list[0].ShouldBe(("someId", 124));
+        }
+
+        [Test]
+        public void Add_WhenItemAdded_ShouldReturnItFirst()
+        {
+            var list = new RecentlyUsedList<int>();
+
+            list.Add("someId", 123);
+            list.Add("someId2", 124);
+
+            list[0].ShouldBe(("someId2", 124));
+        }
     }
 }
--- csharp/tdd-recently-used-list/tdd.recently-used-list/RecentlyUsedList.cs
+++ csharp/tdd-recently-used-list/tdd.recently-used-list/RecentlyUsedList.cs
@@ -1,22 +1,44 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Tdd.Collections
 {
     public class RecentlyUsedList<T> : IRecentlyUsedList<T>
         where T : struct
     {
-        public (string Id, T Value) this[int index] => throw new NotImplementedException();
+        private readonly List<(string Id, T Value)> arr = new List<(string Id, T Value)>();
+
+        // Define the indexer to allow client code to use [] notation.
+        public (string Id, T Value) this[int index] => arr[index];
 
         public int Capacity => throw new NotImplementedException();
 
-        public int Count => throw new NotImplementedException();
+        public int Count
+        {
+            get
+            {
+                return arr.Count;
+            }
+        }
 
-        public void Add(string id, T value) => throw new NotImplementedException();
+        public void Add(string id, T value) {
+            
+            for(int i=0; i<Count; i++)
+            {
+                if(arr[i].Id == id)
+                {
+                    arr.RemoveAt(i);
+                    break;
+                }
+            }
+            
 
+
+            arr.Add((id,value));
+        }
         public IEnumerator<(string Id, T Value)> GetEnumerator() => throw new NotImplementedException();
-
         IEnumerator IEnumerable.GetEnumerator() => throw new NotImplementedException();
     }
 }
