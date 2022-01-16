75be0af2ba5667cf2f1c538d344d9ad008dd0811
tonto7973 <ondrej.urik@gmail.com>
2022-01-16 19:31:53 +0000

    Updated Words to dotnet 6

--- csharp/tdd-words/tdd.words/Words.cs
+++ csharp/tdd-words/tdd.words/Words.cs
@@ -1,25 +1,9 @@
-﻿using System;
-using System.Linq;
-using System.Text;
+﻿namespace Tdd;
 
-namespace tdd.words
+public static class Words
 {
-    public static class Words
+    public static int Count(string sentence)
     {
-        private const string AllowedChars = "0123456789abcdefghijklmnopqrstuvwxyz";
-        public static int Count(string sentence)
-        {
-            if (string.IsNullOrWhiteSpace(sentence))
-            {
-                return 0;
-            }
-
-
-            sentence = sentence.ToLowerInvariant();
-            sentence = new string(sentence.Select(c => AllowedChars.Contains(c) ? c : ' ').ToArray());
-
-            var words = sentence.Trim().Split(" \r\n\t\0".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-            return words.Distinct().Count();
-        }
+        throw new NotImplementedException();
     }
 }
